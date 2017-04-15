700af33feb7299373d0a1bc1ac909195a4ac2354
Li Jianfeng <lee_jianfeng@sjtu.edu.cn>
2017-04-15 14:23:32 +0800

    Import functions in __init__.py

---
+++ pycnf/__init__.py
@@ -0,0 +1,4 @@
+from pycnf import ConfigFile
+from read import load, eval, merge, sections, options
+from write import dump, convert
+from configtype import is_json_file, is_yaml_file, is_ini_file, is_toml_file, get_config_type
